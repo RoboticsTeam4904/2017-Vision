c1fe725ac46a9fcac09546a14ab720d70c7d8ccb
Ethan <ethlu@github.com>
2016-02-03 00:29:59 -0800

    Better subtracted

--- highgoal.cpp
+++ highgoal.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
     cvtColor( src, src_gray, CV_BGR2GRAY );
     blur( src_gray, src_gray, Size(3,3) ); 
     namedWindow( "window", CV_WINDOW_AUTOSIZE );
-    imshow ("window2",src_gray);    
+    imshow ("src_gray",src_gray);    
     createTrackbar( " Threshold:", "window", &thresh, max_thresh, convex_callback );
     createTrackbar( " BlobSize:", "window", &blob_size, max_blob, blob_callback );
     
@@ -59,9 +59,30 @@ void convex_callback(int, void* )
              drawContours(convex, hull, i, Scalar(255,255,255), CV_FILLED, 8, vector<Vec4i>(), 0, Point() );
          }
          
-    subtract(convex, threshold_output, subtracted);
+    subtracted = Mat::zeros(convex.size(), CV_8UC1);
+
+    if(convex.isContinuous()&&threshold_output.isContinuous())
+    {    uchar *p1, *p2, *p3;
+         p1 = convex.ptr<uchar>(0);  
+         p2 = threshold_output.ptr<uchar>(0);
+         p3 = subtracted.ptr<uchar>(0);
+         for (int i=0; i<convex.rows*convex.cols; ++i){
+             if (*p2 != 0){
+                 *p3 = 0;
+             }
+             else if(*p1 != 0){
+                 *p3 = 255;
+             }
+             p1++;
+             p2++;
+             p3++;
+         }
+
+    }
+   // subtract(convex, threshold_output, subtracted);
     imshow("convex", convex);
     imshow("subtracted", subtracted);
+    blob_callback(0,0);
 }
 
 void blob_callback(int, void*)
