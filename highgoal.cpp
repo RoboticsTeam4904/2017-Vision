c4931151c803ce8bee8cde1f159c8af1c30fcb48
widdakay <erik@yoerik.com>
2016-02-15 11:04:46 -0800

    changed gui stuff

--- highgoal.cpp
+++ highgoal.cpp
@@ -2,11 +2,13 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
+#include <dirent.h>
 
 using namespace cv;
 using namespace std;
 
 Mat src, src_gray, subtracted;
+
 int thresh = 200;
 int max_thresh = 255;
 
@@ -15,46 +17,74 @@ int max_blob = 20;
 
 void convex_callback(int, void* );
 void blob_callback(int, void*);
+void analyzeImage(Mat src);
 
-int main(int argc, char* argv[])
+int getdir (string dir, vector<string> &files)
 {
-	cout<<"argc: "<<argc<<endl;
+DIR *dp;
+struct dirent *dirp;
+if((dp = opendir(dir.c_str())) == NULL) {
+cout << "Error(" << errno << ") opening " << dir << endl;
+return errno;
+}
+
+while ((dirp = readdir(dp)) != NULL) {
+files.push_back(string(dirp->d_name));
+}
+closedir(dp);
+return 0;
+}
+
+
+int main(int argc, char* argv[]) {
+
+	
+		cout<<"argc: "<<argc<<endl;
 	if (argc == 2) {
 		cout<<"using '"<<argv[1]<<"' as source"<<endl<<endl;
 		src = imread(argv[1], CV_LOAD_IMAGE_UNCHANGED);
 	} else {
-		cout<<"loading default picture.jpg"<<endl<<endl;
-    	src = imread("picture.jpg", CV_LOAD_IMAGE_UNCHANGED);
-	}
+		
 
-        if (src.empty()) //check whether the image is loaded or not
-     {
-          cout << "Error : Image cannot be loaded..!!" << endl;
-          //system("pause"); //wait for a key press
-          return -1;
+
+     vector<string> files = vector<string>();
+		string dir = "pics/";
+     getdir(dir,files);
+     cout<<"reading"<<endl;
+
+	for (unsigned int i = 0; i < files.size(); i++) {
+
+		
+		cout<<files[i]<<endl;
+		if (files[i].length() < 4 || files[i].substr(files[i].length()-4, 4) != ".jpg") continue;
+		string path = dir + files[i];
+
+		src = imread(path, CV_LOAD_IMAGE_UNCHANGED);
+		if (src.empty()) cout<<"error loading '"<<path<<"'"<<endl;
+     	analyzeImage(src);
+     	
+     	waitKey(0);
      }
+ 	}
 
-    cvtColor( src, src_gray, CV_BGR2GRAY );
-    blur( src_gray, src_gray, Size(3,3) ); 
-    namedWindow( "window", CV_WINDOW_AUTOSIZE );
-    imshow ("src_gray",src_gray);    
-    createTrackbar( " Threshold:", "window", &thresh, max_thresh, convex_callback );
-    createTrackbar( " BlobSize:", "window", &blob_size, max_blob, blob_callback );
-    
-     convex_callback(0,0);
-     blob_callback(0,0);
-
-    waitKey(0);    
+	return 0;
 }
 
-void convex_callback(int, void* )
-{
+void analyzeImage(Mat src) {
+    cvtColor( src, src_gray, CV_BGR2GRAY );
+    blur( src_gray, src_gray, Size(3,3) ); 
+    
+     convex_callback(0,0);
+     blob_callback(0,0);    
+}
+
+void convex_callback(int, void* ) {
     Mat threshold_output, convex;
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
     
     threshold( src_gray, threshold_output, thresh, 255, THRESH_BINARY );
-    imshow("threshold",threshold_output);
+    //imshow("threshold",threshold_output);
     findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
     
     vector<vector<Point> >hull( contours.size() );
@@ -88,8 +118,8 @@ void convex_callback(int, void* )
 
     }
    // subtract(convex, threshold_output, subtracted);
-    imshow("convex", convex);
-    imshow("subtracted", subtracted);
+    //imshow("convex", convex);
+    //imshow("subtracted", subtracted);
     blob_callback(0,0);
 }
 
@@ -102,9 +132,9 @@ void blob_callback(int, void*)
 
         erode(subtracted, blobed, element);
         dilate(blobed, blobed, element);
-        imshow("blobed", blobed);
+        //imshow("blobed", blobed);
         findContours(blobed, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-        Mat result=Mat::zeros(blobed.size(),CV_8UC3);
+        Mat result=src.clone();// Mat::zeros(blobed.size(),CV_8UC3);
 
         for (int i = 0; i<contours.size(); ++i)
              {
