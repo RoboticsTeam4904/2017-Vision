37b2d87e1ccca1353ca22462f22728a196747792
Yoni Lerner <yonilern@gmail.com>
2016-03-10 14:45:24 -0800

    Made constants const

--- highgoal.cpp
+++ highgoal.cpp
@@ -40,20 +40,19 @@ void analyzeImage(Mat src);
 
 pair<float,float> off_angle();
 
-float mountAngleX = 0.0;
-float mountAngleY = 45.0 * M_PI / 180;
-int nativeResX = 2592;
-int nativeResY = 1944;
-float nativeAngleX = 53.5 * M_PI / 180;
-float nativeAngleY = 41.41 * M_PI / 180;
-// All distances are in milimeters
-float shiftX = 336.55; // 13.25 inches
-float shiftY = 57.15; // 2.5 inches
-float goalHeight = 2292.35; // 7.5 feet
-float cameraHeight = 296.0; // 296 milimeters
-float milimetersPerInch = 25.4;
 // CONSTANTS
 // Distances are in milimeters, angles are in degrees
+const float mountAngleX = 0.0;
+const float mountAngleY = 45.0 * M_PI / 180;
+const int nativeResX = 2592;
+const int nativeResY = 1944;
+const float nativeAngleX = 53.5 * M_PI / 180;
+const float nativeAngleY = 41.41 * M_PI / 180;
+const float shiftX = 336.55; // 13.25 inches
+const float shiftY = 57.15; // 2.5 inches
+const float goalHeight = 2292.35; // 7.5 feet
+const float cameraHeight = 296.0; // 296 milimeters
+const float milimetersPerInch = 25.4;
 
 rect_points goal;
 vector<vector<Point> > contours;
