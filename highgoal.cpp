dfdf8faf091c2f33f73a1720f23ab6a49760a5cc
oshlern <oshlern@nuevaschool.org>
2016-03-20 17:55:14 -0700

    Added comments and structured code a bit

--- highgoal.cpp
+++ highgoal.cpp
@@ -3,60 +3,59 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <dirent.h>
+#include <math.h>
 // For availability of M_PI
 #define _USE_MATH_DEFINES
-#include <math.h>
 
 using namespace cv;
 using namespace std;
 
+// Declare vars
 Mat src, src_gray, subtracted;
-
-int thresh = 210;
-int max_thresh = 255;
 int size_x = -1;
 int size_y = -1;
-
-int blob_size = 5;
-int max_blob = 20; // Used only in gui
-
 struct rect_points {
 	Point side_one;
 	Point side_two;
 	Point side_three;
 	Point side_four;
 };
+rect_points goal;
+vector<vector<Point> > contours;
+void convex_callback(int, void*);
+void blob_callback(int, void*);
+void analyzeImage(Mat src);
+pair<float,float> angle_and_dist();
+
+// Create min and max variables for optimal detection based on environment
+int thresh = 210;
+int max_thresh = 255;
+
+int blob_size = 5;
+int max_blob = 20; // Used only in gui
 
 bool gui = false; // Turn on for debugging
 bool detailedGUI = false;
 bool test = false;
 bool latest = false;
 bool done = false;
-bool existingGoal = false;
-
-void convex_callback(int, void*);
-void blob_callback(int, void*);
-void analyzeImage(Mat src);
-
-pair<float,float> off_angle();
+bool foundGoal = false;
 
 // CONSTANTS
-// Distances are in milimeters, angles are in degrees
+// Distances are in inches, angles are in degrees
+const float milimetersPerInch = 25.4;
 const float mountAngleX = 0.0;
 const float mountAngleY = 45.0 * M_PI / 180;
 const int nativeResX = 2592;
 const int nativeResY = 1944;
 const float nativeAngleX = 53.5 * M_PI / 180;
 const float nativeAngleY = 41.41 * M_PI / 180;
-const float shiftX = 336.55; // 13.25 inches
-const float shiftY = 57.15; // 2.5 inches
-const float goalHeight = 2292.35; // 7.5 feet
+const float shiftX = 13.25 * milimetersPerInch; // 13.25 inches
+const float shiftY = 2.5 * milimeters; // 2.5 inches
+const float goalHeight = 8 * 12 * milimetersPerInch; // 8 feet
 const float cameraHeight = 296.0; // 296 milimeters
-const float millimetersPerInch = 25.4;
 
-rect_points goal;
-vector<vector<Point> > contours;
-
+// Access a specified directory, used in one of main's options
 int getdir(string dir, vector<string> &files) {
 	DIR *dp;
 	struct dirent *dirp;
@@ -72,19 +71,16 @@ int getdir(string dir, vector<string> &files) {
 	return 0;
 }
 
-
-
-
 int main(int argc, char** argv) {
 	string image = "latest.jpg";
-
+	// Different args determine different modes, so we can debug or run with ease
+	// Modes are (in order): GUI, testing, using latest.jpg (on RPi), or other directory (uses getDir)
 	if (argc == 1) {
 		detailedGUI = true;
 	} else if (argc == 2) {
 		if (strcmp(argv[1], "test") == 0) {
 			gui = false;
 			test = true;
-
 			cout << "*********************************" << endl;
 			cout << "Testing mode" << endl;
 			cout << "*********************************" << endl << endl;
@@ -129,6 +125,7 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	// If no mode is specified, run the defualt image
 	if (!done) {
 		src = imread(image, CV_LOAD_IMAGE_UNCHANGED);
 		if (src.empty()) {
@@ -140,14 +137,16 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-
+// Process the image specified in main for angle and distance
 void analyzeImage(Mat src) {
 	float offAngle = 0.0;
 	float distance = 0.0;
 	size_x = src.cols;
 	size_y = src.rows;
 
+	// Convert to 1 channel (gray) for use by other operators
 	cvtColor(src, src_gray, CV_BGR2GRAY);
+	// Blur image as to round any small errors
 	blur(src_gray, src_gray, Size(3, 3));
 
 	if (gui) {
@@ -159,12 +158,14 @@ void analyzeImage(Mat src) {
 
 	convex_callback(0, 0);
 
-	if (existingGoal) {
-		pair<float,float> tempvar = off_angle();
+	// Calculate angle and distance if goal is found
+	if (foundGoal) {
+		pair<float,float> tempvar = angle_and_dist();
 		offAngle = tempvar.first;
 		distance = tempvar.second;
 	}
-	cout << existingGoal << "::" << offAngle << "::" << distance << endl;
+	// Print results for debugging or communication with RIO
+	cout << foundGoal << "::" << offAngle << "::" << distance << endl;
 	if (gui) waitKey(0);
 }
 
@@ -173,10 +174,15 @@ void convex_callback(int, void*) {
 	Mat threshold_output, convex;
 	vector<Vec4i> hierarchy;
 
+	// Convert to only black and white pixels using threshold for use by findcontours
 	threshold(src_gray, threshold_output, thresh, max_thresh, THRESH_BINARY);
 	if (gui && detailedGUI) imshow("threshold", threshold_output);
+	// Find shapes for use by other operators
 	findContours(threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
+	// Blob Filter for pre conxev hull?
+
+	// Create convex hulls of contours for detecting the high goal's U shape
 	vector<vector<Point> > hull(contours.size());
 	for(int i = 0; i < contours.size(); ++i) {
 		convexHull(Mat(contours[i]), hull[i], false);
@@ -187,8 +193,8 @@ void convex_callback(int, void*) {
 		drawContours(convex, hull, i, Scalar(255,255,255), CV_FILLED, 8, vector<Vec4i>(), 0, Point() );
 	}
 
+	// Subtract the original contours from convex hulls, remaining with the inner part of the U
 	subtracted = Mat::zeros(convex.size(), CV_8UC1);
-
 	if (convex.isContinuous() && threshold_output.isContinuous()) {
 		uchar *p1, *p2, *p3;
 		p1 = convex.ptr<uchar>(0);
@@ -204,7 +210,6 @@ void convex_callback(int, void*) {
 			p2++;
 			p3++;
 		}
-
 	}
 	// subtract(convex, threshold_output, subtracted);
 
@@ -212,6 +217,7 @@ void convex_callback(int, void*) {
 		imshow("convex", convex);
 		imshow("subtracted", subtracted);
 	}
+	// Detect highgoal from whats left and create a goal object
 	blob_callback(0, 0);
 }
 
@@ -221,6 +227,7 @@ void blob_callback(int, void*) {
 	Mat blobbed;
 	Mat element = getStructuringElement(MORPH_ELLIPSE, Size(2 * blob_size + 1, 2 * blob_size + 1), Point(blob_size, blob_size));
 
+	// Grow and shrink shape to get better edges
 	erode(subtracted, blobbed, element);
 	dilate(blobbed, blobbed, element);
 	if (gui && detailedGUI) imshow("blobbed", blobbed);
@@ -228,8 +235,9 @@ void blob_callback(int, void*) {
 	Mat result = src.clone();
 	// Mat::zeros(blobbed.size(), CV_8UC3);
 
+	// Filter for largest
 	if (contours.size()!=0) {
-		existingGoal = true;
+		foundGoal = true;
 		double largest_area = 0.0;
 		for (int i = 0; i < contours.size(); i++) {
 			// Find the area of contour
@@ -240,6 +248,7 @@ void blob_callback(int, void*) {
 			}
 		}
 	}
+	// Create goal
 	approxPolyDP(Mat(largest_contour), poly, 3, true);
 	goal.side_one = poly[0];
 	goal.side_two = poly[1];
@@ -255,7 +264,8 @@ void blob_callback(int, void*) {
 	}
 }
 
-pair<float,float> off_angle() {
+// Calculate angle and distance based off of pixel coordinates. Return radians (straight ahead - 0, right - positive, left - negative), distance (on the floor from shooter to middle of U on highgoal)
+pair<float,float> angle_and_dist() {
 	float degPerPxlX = nativeAngleX / size_x;
 	float degPerPxlY = nativeAngleY / size_y;
 	float goalPixelY = size_y - (goal.side_two.y + goal.side_one.y + goal.side_three.y + goal.side_four.y) / 4;
