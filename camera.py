0851cb97030785b592cc8431ea15cd2a8e864ddb
Osher Lerner <oshlern@nuevaschool.org>
2017-01-09 19:10:14 -0800

    Removed continuous stream from camera.getImage

--- camera.py
+++ camera.py
@@ -11,7 +11,6 @@ rawCapture = PiRGBArray(camera, size=camera.resolution)
 camera.exposure_mode = 'sports'
 
 # constants
-# cameraResolution = (640, 480)
 camera.nativeResolution = (2592, 1944)
 camera.nativeAngle = (math.radians(53.5), math.radians(41.41))
 camera.mountAngle = (0, math.radians(45))
@@ -20,43 +19,12 @@ camera.goalHeight = 8 * 12
 camera.height = 296 / 25.4 #to inches
 
 def getImage():
-	image = None
-	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-		# grab the raw NumPy array representing the image, then initialize the timestamp
-		# and occupied/unoccupied text
-		image = frame.array
+	# for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	frame = camera.capture(rawCapture, format="bgr", use_video_port=True):
 
-		# clear the stream in preparation for the next frame
-		rawCapture.truncate(0)
-		return image
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
 
-# cnt = 1
-# def getImage(webcam=False, useLatestImage=False, useStoredImage=False): # pi camera is default
-# 	image = None
-# 	if webcam:
-# 		ret, image = cap.read()
-# 		#cv2.imwrite("/Users/erik/"+str(time.time())+".jpg", image)
-#
-# 	elif useLatestImage:
-# 		image = cv2.imread("latest.jpg")
-#
-# 	elif useStoredImage:
-# 		cnt += 1
-# 		print cnt
-# 		image = cv2.imread("4/a{0:05d}.jpg".format(cnt))
-# 		while(image == None and cnt < 10000):
-# 			cnt += 1
-# 			image = cv2.imread("4/a{0:05d}.jpg".format(cnt))
-# 		if not cnt < 10000:
-# 			cnt = 0
-# 			image = getImage()
-#
-# 	else: # from pi camera
-# 		for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-# 			# grab the raw NumPy array representing the image, then initialize the timestamp
-# 			# and occupied/unoccupied text
-# 			image = frame.array
-#
-# 			# clear the stream in preparation for the next frame
-# 			rawCapture.truncate(0)
-# 	return image
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	return frame.array
