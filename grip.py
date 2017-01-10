0643b4519c077eb2484a807269fce6cb1312002c
Shaheen Cullen-Baratloo <shacull@nuevaschool.org>
2017-01-09 17:52:57 -0800

    Customizations for use on the pi.

--- grip.py
+++ grip.py
@@ -116,7 +116,11 @@ class GripPipeline:
         else:
             mode = cv2.RETR_LIST
         method = cv2.CHAIN_APPROX_SIMPLE
-        contours, hierarchy = cv2.findContours(input, mode=mode, method=method)
+
+        if pi:
+            contours, hierarchy = cv2.findContours(input, mode=mode, method=method)
+        else:
+            im1, contours, hierarchy = cv2.findContours(input, mode=mode, method=method)
         return contours
 
     @staticmethod
