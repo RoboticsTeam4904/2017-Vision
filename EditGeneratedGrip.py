54fe49d533676d59f09e410a96c363c1833ba477
Osher Lerner <oshlern@nuevaschool.org>
2017-01-14 11:35:58 -0800

    Tested editCode and made it nicer.

--- EditGeneratedGrip.py
+++ EditGeneratedGrip.py
@@ -14,6 +14,9 @@ def saveCode(doc, code):
 def editCode(doc):
     code = openCode(doc)
     if not3:
-        code = re.sub('im2, ', '', code)
-    code = re.sub('\n from enum import Enum', '', code)
+        # code = re.sub('im2, ', '', code)
+        code = re.sub('im2, contours, hierarchy =cv2.findContours', 'contours, hierarchy =cv2.findContours', code)
+    code = re.sub('import numpy\nimport math\nfrom enum import Enum', '#import numpy\n#import math\n#from enum import Enum', code)
     saveCode(doc, code)
+
+# editCode('grip.py')
