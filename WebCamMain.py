dab0b28b0a866cdccc9e764003b970d5ad7a82f5
CaseyManning <kcgrind@gmail.com>
2017-01-22 14:23:43 -0800

    centralized constants

--- WebCamMain.py
+++ WebCamMain.py
@@ -14,19 +14,8 @@ from NetworkTabling import publishToTables, initializeTables
 import CameraLogic
 from Printing import printResults
 import GripRunner
+from config import *
 
-
-debug = True
-edited = False
-adjustCoords = True
-withOpenCV3 = True
-
-resolution = (640, 360)
-exposure = 1
-ip = "10.49.4.2"
-team = 4904
-gripDoc = "grip.py"
-sampleImage = "TestImages/GearTest.png"
 
 if adjustCoords:
 	halfWidth = resolution[0]/2
---
+++ config.py
@@ -0,0 +1,11 @@
+debug = True
+edited = False
+adjustCoords = True
+withOpenCV3 = True
+
+resolution = (640, 360)
+exposure = 1
+ip = "10.49.4.2"
+team = 4904
+gripDoc = "grip.py"
+sampleImage = "TestImages/GearTest.png"
