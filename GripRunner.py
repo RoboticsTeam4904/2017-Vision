4810101654d37d8c9cde4298d8ea467cba5df40b
CaseyManning <kcgrind@gmail.com>
2017-01-22 11:13:47 -0800

    cleaned up GripRunner.py

--- GripRunner.py
+++ GripRunner.py
@@ -20,7 +20,6 @@ pi = False
 webcam = False
 
 debug = True
-continuous = True
 edited = False
 adjustCoords = True
 withOpenCV3 = True
@@ -35,9 +34,6 @@ sampleImage = "TestImages/GearTest.png"
 if adjustCoords:
 	halfWidth = resolution[0]/2
 
-if not pi and not webcam:
-	continuous = False
-
 def main():
 	pipeline = initializeGrip(gripDoc)
 
@@ -48,15 +44,10 @@ def main():
 	except:
 		network = None
 
-	while continuous:
+	while webcam or pi:
 		runVision(camera, network, pipeline) #count frame nums if necessary
 	runVision(camera, network, pipeline)
 
-	# if continuous:
-	# 	while True:
-	# 		runVision(camera, network, pipeline) #count frame nums if necessary
-	# else:
-	# 	runVision(camera, network, pipeline)
 
 def runVision(camera, network, pipeline):
 
@@ -64,6 +55,7 @@ def runVision(camera, network, pipeline):
 	pipeline.process(image)
 	targets = filterContours(pipeline.filter_contours_output, debug) # To be edited if the last filter is changed in case of algorithmic changes. 
 	center = findCenter(targets) #if 2, join and find center, if 1, return val, if 0 return input. if adjustCoords:	center[0] -= halfWidth
+	
 	if debug:
 		printResults(pipeline.filter_contours_output, center, image)
 	try:
@@ -78,46 +70,6 @@ def initializeGrip(doc):
 		EditGeneratedGrip.editCode(doc, withOpenCV3=withOpenCV3)
 	from grip import GripVisionPipeline  # TODO change the default module and class, if needed
 	return GripVisionPipeline()
-
-	# total_contour = np.concatenate((largest_contour, second_largest_contour))
-		# x, y, w, h = cv2.boundingRect(total_contour) # Works best when camera is horizontal relative to target
-
-		# center = (x+w/2, y+h/2)
-		# if debug:
-		# 	print "Found Center:", center
-		# 	cv2.drawContours(image, contours, -1, (70,70,0), 3)
-		# 	cv2.drawContours(image, [largest_contour], -1, (0,255,0), 3)
-		# 	cv2.drawContours(image, [second_largest_contour], -1, (0,0,255), 3)
-		# 	cv2.drawContours(image, [total_contour], -1, (255,0,0), 3)
-		# 	cv2.circle(image, center, 4, (255, 255, 255))
-		# 	cv2.imshow("Contours Found", image)
-		# 	cv2.waitKey(0)
-		# 	cv2.destroyAllWindows()
-		# return center
-	
-	# 	x, y, w, h = cv2.boundingRect(contours[0])
-	# 	center = (x+w/2, y+h/2)
-	# 	if debug:
-	# 		print "Found Center:", center
-	# 		print "1 contour found (no bueno)"
-	# 		cv2.drawContours(image, contours, -1, (70,70,0), 3)
-	# 		cv2.circle(image, center, 4, (255, 255, 255))
-	# 		cv2.imshow("Contours Found", image)
-	# 		cv2.waitKey(0)
-	# 		cv2.destroyAllWindows()
-	# 	return center
-	# else:
-	# 	if debug:
-	# 		print "RIP. no contours."
-	# 	return (0,0)
-
-	# if debug:
-	# 	print "Got image. Analyzing image (pipeline process)..."
-	# 	print "Image processed. Analyzing contours..."
-	# 	print "Analyzed. Publishing to network tables..."
-
-	
-	
 
 if __name__ == '__main__':
 	main()
