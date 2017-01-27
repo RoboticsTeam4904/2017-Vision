ea8c2dd5e61d683c91c279ca9b283ea2cf20d223
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2017-01-27 14:55:45 -0800

    Organized config and enabled setting other values

--- WebCam.py
+++ WebCam.py
@@ -6,12 +6,18 @@ def getImage():
 	retval, image = camera.read()
 	return image
 
-def set(resolution=False, exposure=False):
+def set(resolution=False, exposure=False, gain=False, contrast=False):
 	if resolution:
 		camera.set(3, resolution[0])
 		camera.set(4, resolution[1])
 	if exposure:
-		subprocess.call("/usr/bin/v4l2-ctl -d /dev/video0 -c exposure_auto=1 -c exposure_auto_priority=0 -c exposure_absolute="+str(exposure)+" -c gain=10 -c contrast=50", shell=True)
+		subprocess.call("/usr/bin/v4l2-ctl -d /dev/video0 -c exposure_auto=1 -c exposure_auto_priority=0 -c exposure_absolute={}".format(exposure), shell=True)
+	if gain:
+		subprocess.call("/usr/bin/v4l2-ctl -d /dev/video0 -c gain={}".format(gain), shell=True)
+	if contrast:
+		subprocess.call("/usr/bin/v4l2-ctl -d /dev/video0 -c contrast={}".format(contrast), shell=True)
+
+
 
 def getExposure():
 	return camera.get(15)
--- config.py
+++ config.py
@@ -1,12 +1,20 @@
+# Logging/debug settings
+# --------------
 debug = True
 extra_debug = False
 save = False
 display = True
 
+# Camera settings
+# --------------
 exposure = 12
+gain = 10
+contrast = 50
 resolution = (640, 360)
 halfWidth = resolution[0]/2
 
+# Misc
+# -------------
 ip = "10.49.4.2"
 team = 4904
 withOpenCV3 = False
