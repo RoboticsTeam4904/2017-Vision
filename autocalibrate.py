5019ee50f626b49992bf286b1f09c548d2d37c8a
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2017-01-27 15:32:53 -0800

    Refactored camera

--- autocalibrate.py
+++ autocalibrate.py
@@ -2,13 +2,14 @@ import numpy as np
 import GripRunner
 import Printing 
 import config
+import WebCam
 
-def autocalibrate(camera):
-	print "Calibrating camera... (may not work)"
-	exposure = camera.getExposure()
+def autocalibrate():
+	print "Calibrating WebCam... (may not work)"
+	exposure = WebCam.getExposure()
 	numContours = 0
 	while numContours != 2:
-		image = camera.getImage()
+		image = WebCam.getImage()
 		contours = GripRunner.run(image)
 		numContours = len(contours)
 		print numContours, exposure
@@ -17,11 +18,14 @@ def autocalibrate(camera):
 			Printing.drawContours(image, contours)
 			Printing.display(image)
 		exposure = np.multiply(exposure, np.true_divide(2+randomVar, numContours+randomVar)) #np.true_divide(sqrtTwo, np.sqrt(numContours)
-		camera.set(exposure=exposure)
+		WebCam.set(exposure=exposure)
 	# get range of exposure
 
 
-# 	shutter_speed = camera.getShutterSpeed()
+# 	shutter_speed = WebCam.getShutterSpeed()
 
 # 		shutter_speed = np.multiply(shutter_speed, np.true_divide(numContours+randomVar, 2+randomVar)) #np.true_divide(sqrtTwo, np.sqrt(numContours)
-# 		camera.set(shutter_speed=shutter_speed)
+# 		WebCam.set(shutter_speed=shutter_speed)
+ 
+if __name__ == '__main__':
+	autocalibrate()
