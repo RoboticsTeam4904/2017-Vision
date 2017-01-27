d6f89679110b776f5e1ae0c7f9cbed00b73fc65c
Osher Lerner <oshlern@nuevaschool.org>
2017-01-27 12:27:20 -0800

    reimplemented isVisible in network tables

--- NetworkTabling.py
+++ NetworkTabling.py
@@ -10,17 +10,16 @@ def publishToTables(center, frameNum=0, distance=0):
 	isVisible = False
 	if center:
 		isVisible = True
-		#if adjustCoords:
-		#center[0] = center[0] - halfWidth
-		#center = (center[0] - halfWidth, center[1])
+		center = (center[0] - halfWidth, center[1])
+		
+	else:
 		isVisible = False
-		#center = (0,0)
-		print center
-		table.putNumber('centerX', center[0])
-		table.putNumber('centerY', center[1]) # Can be deleted
-		#table.putBool('isVisible', isVisible)
-		table.putNumber('frameNum', frameNum)
-		table.putNumber('distance', distance) # Feet away
+		center = (0,0)
+	table.putNumber('centerX', center[0])
+	table.putNumber('centerY', center[1]) # Can be deleted
+	table.putBoolean('isVisible', isVisible)
+	table.putNumber('frameNum', frameNum)
+	table.putNumber('distance', distance) # Feet away
 
 	# if debug:
 		# print "Published to network tables."
