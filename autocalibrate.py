472c247d7a98b97e505abb0bf999424e26203946
TX1 <nachiketa.gargi@gmail.com>
2017-02-02 18:55:52 -0800

    made calibration bueno and work. Still need to add outputs

--- autocalibrate.py
+++ autocalibrate.py
@@ -2,8 +2,8 @@ import numpy as np
 import GripRunner, Printing, WebCam, config
 import cv2, time
 
-minExposure = 3
-maxExposure = 1000
+minExposure = 5
+maxExposure = 100
 resolutionArea = 600000
 maxArea = resolutionArea/4
 targetAverageValue = 30
@@ -23,23 +23,24 @@ def autocalibrate():
 	print "Calibrating WebCam... (may not work)"
 	currentTime = time.clock()
 	exposure = WebCam.getExposure()
-	print exposure
-	newTime = time.clock()
-	getExposureTime = np.subtract(newTime, currentTime)
-	currentTime = time.clock()
-	brightIters = []
-	runTimes = []
-	restTimes = []
+	print "exposure", exposure
+#	newTime = time.clock()
+#	getExposureTime = np.subtract(newTime, currentTime)
+#	currentTime = time.clock()
+#	brightIters = []
+#	runTimes = []
+#	restTimes = []
 	b = time.clock()
-	for i in range(1000):
+	for i in range(20):
 #		currentTime = time.clock()
 		image = WebCam.getImage()
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-		value = cv2.split(image)[2]
+		img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+		value = cv2.split(img)[2]
 		average = cv2.mean(value)[0]
-#		if config.display:
-#			Printing.display(image)
+		if config.display:
+			Printing.display(image)
 		if np.absolute(np.subtract(average, targetAverageValue)) < averageValueThreshold:
+			print i, "num iterations, bright"
 			break
 		scaleBy = np.divide(targetAverageValue, average)
 		exposure = np.minimum(np.maximum(np.multiply(exposure, scaleBy), minExposure), maxExposure)
@@ -54,32 +55,36 @@ def autocalibrate():
 		image = WebCam.getImage()
 #		tempTime = time.clock()
 		contours = GripRunner.run(image)
-#		currentTime = time.clock()
+		currentTime = time.clock()
 #		runTimes += [np.subtract(currentTime, tempTime)]
 		numContours = len(contours)
 #		print numContours, exposure, WebCam.getExposure()
+		if i%50:
+                        print i, numContours
 		if numContours > 0:
 			if tooLarge(contours):
 				exposure = np.divide(exposure, 10)
 		if numContours == 2:
 			numGoodFrames += 1
 			if numGoodFrames == numTests:
+				print i, "num iterations grip"
 				# image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 				# value = cv2.split(image)[2]
 				# average = cv2.mean(value)[0]
 				# print average
 				print "DID IT___________"
-				# return True
+				print "grip", time.clock() - s
+				return True
 		else:
 			numGoodFrames = 0
 		randomVar = np.random.random_sample()
 		scaleBy = np.true_divide(2+randomVar, numContours+randomVar)
 		exposure = np.minimum(np.maximum(np.multiply(exposure, scaleBy), minExposure), maxExposure)
 		WebCam.set(exposure=exposure)
-		restTimes += [np.subtract(time.clock(),currentTime)]
-#		if config.display:
-#			Printing.drawContours(image, contours)
-#			Printing.display(image)
+#		restTimes += [np.subtract(time.clock(),currentTime)]
+		if config.display:
+			Printing.drawContours(image, contours)
+			Printing.display(image)
 #	print "getExposureTime", getExposureTime
 #	print "bright", np.sum(brightIters), np.average(brightIters), np.round(brightIters, decimals=2)
 #	print "run", np.sum(runTimes), np.average(runTimes), np.round(runTimes, decimals=2)
@@ -98,7 +103,7 @@ def tooLarge(contours):
 
 
 def displace():
-	WebCam.set(exposure=1000)
+	WebCam.set(exposure=2000)
 
 # 	shutter_speed = WebCam.getShutterSpeed()
 
@@ -118,4 +123,12 @@ def displace():
 if __name__ == '__main__':
 	start = time.clock()
 	autocalibrate()
-	print time.clock() - start
+#	WebCam.set(exposure=5)
+	print WebCam.getExposure()
+	print time.clock() - start, "TOTAL TIME"
+	while config.display:
+		image = WebCam.getImage()
+		contours = GripRunner.run(image)
+		Printing.drawContours(image, contours)
+		Printing.display(image)
+		cv2.waitKey(20)
