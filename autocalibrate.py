ef8c3183c5890ce7d7a3a876d8d3ebf1b235c338
Osher Lerner <oshlern@nuevaschool.org>
2017-02-02 16:21:19 -0800

    np bug fixes and stand-alone file for testing

--- autocalibrate.py
+++ autocalibrate.py
@@ -9,28 +9,57 @@ minExposure = 3
 maxExposure = 1000
 resolutionArea = 600000
 maxArea = resolutionArea/4
+targetAverageValue = 50
+averageValueThreshold = 30
+numTests = 5
 # test image max area is 58106.0
+
+# image = cv2.imread('TestImages/Cancer.jpg')
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+# value = cv2.split(image)[2]
+# # value = np.array([image[:,:,2]])
+# average = cv2.mean(value)
+# print average
 
 def autocalibrate():
 	print "Calibrating WebCam... (may not work)"
 	exposure = WebCam.getExposure()
-	numContours = 0
-	while numContours != 2:
+	for i in range(10):
+		image = WebCam.getImage()
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+		value = cv2.split(image)[2]
+		average = cv2.mean(value)
+		if config.display:
+			Printing.display(image)
+		if np.absolute(np.subtract(average, targetAverageValue)) < averageValueThreshold:
+			break
+		scaleBy = np.divide(targetAverageValue, average)
+		exposure = np.minimum(np.maximum(np.multiply(exposure, scaleBy), minExposure), maxExposure)
+		WebCam.set(exposure=exposure)
+
+	numGoodFrames = 0
+	for i in range(20):
 		image = WebCam.getImage()
 		contours = GripRunner.run(image)
 		numContours = len(contours)
 		print numContours, exposure, WebCam.getExposure()
+		if tooLarge(contours):
+			exposure = np.divide(exposure, 10)
+		if numContours == 2:
+			numGoodFrames += 1
+			if numGoodFrames == numTests:
+				return True
+		else:
+			numGoodFrames = 0
 		randomVar = np.random.random_sample()
+		scaleBy = np.true_divide(2+randomVar, numContours+randomVar)
+		exposure = np.minimum(np.maximum(np.multiply(exposure, scaleBy), minExposure), maxExposure)
+		WebCam.set(exposure=exposure)
 		if config.display:
 			Printing.drawContours(image, contours)
 			Printing.display(image)
-		exposure = np.multiply(exposure, np.true_divide(2+randomVar, numContours+randomVar)) #np.true_divide(sqrtTwo, np.sqrt(numContours)
-		if tooLarge(contours):
-			exposure = np.multiply(exposure, 0.1)
-		exposure = np.min(np.max(exposure, minExposure), maxExposure)
-		WebCam.set(exposure=exposure)
-	# get range of exposure
-	Printing.save(image, name="autocalibrate")
+	return false
+
 
 def tooLarge(contours):
 	areas = [cv2.contourArea(contour, False) for contour in contours]
@@ -46,5 +75,15 @@ def tooLarge(contours):
 # 		shutter_speed = np.multiply(shutter_speed, np.true_divide(numContours+randomVar, 2+randomVar)) #np.true_divide(sqrtTwo, np.sqrt(numContours)
 # 		WebCam.set(shutter_speed=shutter_speed)
  
-if __name__ == '__main__':
-	autocalibrate()
+# if numContours == 2:
+# 			for i in range(numTests):
+# 				image = WebCam.getImage()
+# 				contours = GripRunner.run(image)
+# 				numContours = len(contours)
+# 				if numContours != 2:
+# 					break
+# 			else:
+# 				break
+
+# if __name__ == '__main__':
+# 	autocalibrate()
---
+++ testPhoto.py
@@ -0,0 +1,17 @@
+import WebCam, Printing, GripRunner, ContourFinding, SpikeFinding
+n = 0
+def d():
+	while True:
+		image = WebCam.getImage()
+		contours = GripRunner.run(image)
+		targets = ContourFinding.filterContours(contours) # To be edited if the last filter is changed in case of algorithmic changes. 
+		center, distance = SpikeFinding.findCenterandDist(targets) #if 2, join and find center, if 1, return val, if 0 return input. if adjustCoords:	center[0] -= halfWidth
+		# Printing.printResults(contours, center, distance)
+		Printing.drawImage(image, contours, targets, center)
+		Printing.display(image)
+
+def s():
+	global n
+	image = WebCam.getImage()
+	Printing.save(image, name="TEST" + str(n))
+	n += 1
