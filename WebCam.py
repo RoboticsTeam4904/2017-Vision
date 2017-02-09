38e3aadd0db544f158821ea38f25507ec038342c
CaseyManning <kcgrind@gmail.com>
2017-02-08 16:33:40 -0800

    Merge WebCam.py from master into robust
    maybe?

--- WebCam.py
+++ WebCam.py
@@ -6,10 +6,8 @@ def getImage():
 	retval, image = camera.read()
 	return image
 
-def set(resolution=False, exposure=False, gain=False, contrast=False):
+def set(exposure=False, gain=False, contrast=False):
 	settingStr = "/usr/bin/v4l2-ctl -d /dev/video0"
-	if resolution:
-		settingStr += " -c --set-fmt-video=width={},height={}".format(resolution[0], resolution[1]) #
 	if exposure:
 		settingStr += " -c exposure_auto=1 -c exposure_auto_priority=0 -c exposure_absolute={}".format(exposure)
 	if gain:
