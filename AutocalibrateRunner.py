978ea1817bd679b4f6d7868720299f2aa3351d97
Osher Lerner <oshlern@nuevaschool.org>
2017-03-11 13:25:17 -0800

    fixed calibration runner. Tested

--- AutocalibrateRunner.py
+++ AutocalibrateRunner.py
@@ -1,20 +1,15 @@
 from networktables import NetworkTables
 from autocalibrate import calibrate
-from config import ip, team
-NetworkTables.setTeam(team)
-NetworkTables.initialize(server=ip)
-table = NetworkTables.getTable('autocalibrate')
-#in the future, use network tables from networkTabling in report-autocalibrate branch
+import config
+NetworkTables.setTeam(config.team)
+NetworkTables.initialize(server=config.ip)
+table = NetworkTables.getTable('Vision')
 
 calibrations = 0
-
-print "Starting Button Autocalibration!"
 while True:
-	robotPresses = table.getNumber('autocalibrate', -1)
-	print "robotPresses is ", robotPresses
+	robotPresses = table.getNumber('Autocalibration count', -1)
 	if robotPresses > calibrations:
-		print "For some reason, we are calibrating the camera"
+		print "Calibrating the camera due to button press"
 		calibrations += 1
 		calibrate()
-		table.putBoolean('didCalibrate', True)
 
