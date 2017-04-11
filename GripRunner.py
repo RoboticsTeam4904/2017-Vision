4b9e5770de8525c9b270a6e0b3a4c5bb4a8f6610
oshlern <oshlern@nuevaschool.org>
2017-04-11 11:34:34 -0700

    reorganized grip editing (always run)

--- GripRunner.py
+++ GripRunner.py
@@ -1,4 +1,19 @@
-pipeline = False
+import re,  config
+
+# This code was not what we planned to write; in fact, we still cringe at its sight
+# But, seeing how our code is a little rudamentary, we are forced to add some artificial intelligence.
+# Sadly, we have written code that transcends programming itself. This code writes its own code.
+# This horrid beast has taken our jobs and our enthusiasm. If we never code again, know it was because of this function.
+# So Team 4904 presents you the next level of programming:
+code = open('grip.py', 'r').read()
+if config.withOpenCV3:
+	code = re.sub('    contours, hierarchy =cv2.findContours', '    im2, contours, hierarchy =cv2.findContours', code)
+else:
+	code = re.sub('im2, contours, hierarchy =cv2.findContours', 'contours, hierarchy =cv2.findContours', code)
+open('grip_edited.py', 'w').write(code)
+
+from grip_edited import GripPipeline
+pipeline = GripPipeline()
 
 def run(image):
 	pipeline.process(image)
@@ -11,25 +26,3 @@ def calibrate(hsv=False, area=False):
 		pipeline.__hsv_threshold_value = hsv[2]
 	if area:
 		pipeline.__filter_contours_min_area = area
-
-
-# This code was not what we planned to write; in fact, we still cringe at its sight
-# But, seeing how our code is a little rudamentary, we are forced to add some artificial intelligence. 
-# Sadly, we have written code that transcends programming itself. This code writes its own code. 
-# This horrid beast has taken our jobs and our enthusiasm. If we never code again, know it was because of this function.
-# So Team 4904 presents you the next level of programming:
-def editedPipeline():
-    import re,  config
-    if config.editGrip:
-    	code = open('grip.py', 'r').read()
-	    if config.withOpenCV3:
-	        code = re.sub('    contours, hierarchy =cv2.findContours', '    im2, contours, hierarchy =cv2.findContours', code)
-	    else:
-	        code = re.sub('im2, contours, hierarchy =cv2.findContours', 'contours, hierarchy =cv2.findContours', code)
-	    open('grip_edited.py', 'w').write(code)
-    	from grip_edited import GripPipeline
-    else:
-		from grip import GripPipeline	    
-    return GripPipeline()
-
-pipeline = editedPipeline()
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import numpy as np
 debug = False
 extra_debug = False #step through each contour in the webcam by turning on extra_debug
 display = False
-autocalibrate = False
+autocalibrate = False # REMOVE
 save = True
 saveFrequency = 50
 
@@ -15,10 +15,10 @@ exposure = 90
 gain = 10
 contrast = 50
 displacement = 4.25/12.0 # Vertical feet from camera to bottom of vision target
-cameraTilt = 0
-width = 8.25/12 #from centers. targets are 2x5 inches and 6.25 inches apart
+cameraTilt = 0 # REORGANIZE CONSTANTS INTO APPROPRIATE FILES
+width = 8.25/12.0 #from centers. targets are 2x5 inches and 6.25 inches apart
 nativeAngle  = (np.radians(64), np.radians(48)) #experimentally determined 10 pxl per deg at 640x480, going down by a v smol amount at the edge of the frame
-def setResolution(res):
+def setResolution(res): # REMOVE NEW FUNCTION
 	global resolution, degPerPxl
 	resolution = res
 	degPerPxl = np.divide(nativeAngle, resolution)
@@ -29,6 +29,5 @@ setResolution((640, 480))
 ip = "10.49.4.2"
 team = 4904
 withOpenCV3 = int(cv2.__version__[0]) == 3
-editGrip = True
 sampleImage = "TestImages/GearTest.png"
 
