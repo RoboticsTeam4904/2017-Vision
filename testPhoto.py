ddfa02d275e91427ba92302b74707976caec8f0f
oshlern <oshlern@nuevaschool.org>
2017-04-18 17:58:51 -0700

    fixed testPhoto

--- testPhoto.py
+++ testPhoto.py
@@ -1,4 +1,4 @@
-import WebCam, Printing, GripRunner, ContourFinding, SpikeFinding, autocalibrate
+import PiCamera, Printing, GripRunner, ContourFinding, SpikeFinding, autocalibrate
 n = 0
 def d():
 	while True:
@@ -6,22 +6,18 @@ def d():
 
 def s():
 	global n
-	image = WebCam.getImage()
+	image = PiCamera.getImage()
 	Printing.save(image, name="TEST" + str(n))
 	n += 1
 
 def e(exposure):
-	WebCam.setCamera(exposure=exposure)
+	PiCamera.setCamera(shutter_speed=exposure)
 
 def o():
-	image = WebCam.getImage()
+	image = PiCamera.getImage()
 	contours = GripRunner.run(image)
-	targets = ContourFinding.filterContours(contours) # To be edited if the last filter is changed in case of algorithmic changes.
+	targets = ContourFinding.filterContoursByArea(contours) # To be edited if the last filter is changed in case of algorithmic changes.
 	# center, distance = SpikeFinding.findCenterandDist(targets) #if 2, join and find center, if 1, return val, if 0 return input. if adjustCoords:	center[0] -= halfWidth
 	# # Printing.printResults(contours, center, distance)
 	Printing.drawImage(image, contours, targets)
 	Printing.display(image)
-
-def a():
-	autocalibrate.calibrate()
-	print WebCam.getExposure()
